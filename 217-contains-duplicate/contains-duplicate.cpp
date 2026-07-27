0eb3fe77a8e6b25af8d6ac33d7da02cb3c7e0b22
Janvi-0 <149682461+Janvi-0@users.noreply.github.com>
2026-07-27 22:23:57 +0530

    Time: 75 ms (28.03%) | Memory: 111.2 MB (32.32%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i]==nums[i-1]) return true;
+        unordered_map<int,int> freq;
+        for(int x : nums) {
+            freq[x]++;
+            if(freq[x] > 1)
+                return true;
         }
         return false;
     }
