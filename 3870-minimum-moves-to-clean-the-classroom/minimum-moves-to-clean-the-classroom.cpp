e1b1c6964b3372165689ffb7197b19bf303cd113
Janvi-0 <149682461+Janvi-0@users.noreply.github.com>
2026-09-02 02:57:57 +0530

    Time: 211 ms (86.00%) | Memory: 143.5 MB (76.00%) - LeetSync

---
+++ 3870-minimum-moves-to-clean-the-classroom/minimum-moves-to-clean-the-classroom.cpp
@@ -0,0 +1,98 @@
+class Solution {
+public:
+    int minMoves(vector<string>& classroom, int energy) {
+        int m = classroom.size();
+        int n = classroom[0].size();
+
+        int sr = -1, sc = -1;
+        int litterCount = 0;
+
+        vector<vector<int>> id(m, vector<int>(n, -1));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (classroom[i][j] == 'S') {
+                    sr = i;
+                    sc = j;
+                }
+                else if (classroom[i][j] == 'L') {
+                    id[i][j] = litterCount++;
+                }
+            }
+        }
+
+        int fullMask = (1 << litterCount) - 1;
+
+        // best[r][c][mask] = maximum energy with which
+        // we have reached this state
+        vector<vector<vector<int>>> best(
+            m,
+            vector<vector<int>>(n, vector<int>(1 << litterCount, -1))
+        );
+
+        // {row, col, mask, remaining energy}
+        queue<array<int, 4>> q;
+
+        q.push({sr, sc, 0, energy});
+        best[sr][sc][0] = energy;
+
+        int moves = 0;
+
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        while (!q.empty()) {
+            int sz = q.size();
+
+            while (sz--) {
+                auto [r, c, mask, e] = q.front();
+                q.pop();
+
+                if (mask == fullMask)
+                    return moves;
+
+                // If no energy remains, cannot make another move.
+                if (e == 0)
+                    continue;
+
+                for (int d = 0; d < 4; d++) {
+                    int nr = r + dr[d];
+                    int nc = c + dc[d];
+
+                    if (nr < 0 || nr >= m ||
+                        nc < 0 || nc >= n)
+                        continue;
+
+                    if (classroom[nr][nc] == 'X')
+                        continue;
+
+                    int ne = e - 1;
+                    int nmask = mask;
+
+                    // Collect litter
+                    if (classroom[nr][nc] == 'L') {
+                        nmask |= (1 << id[nr][nc]);
+                    }
+
+                    // Reset energy
+                    if (classroom[nr][nc] == 'R') {
+                        ne = energy;
+                    }
+
+                    // Dominance pruning:
+                    // If we have already reached this state
+                    // with >= energy, this state is useless.
+                    if (best[nr][nc][nmask] >= ne)
+                        continue;
+
+                    best[nr][nc][nmask] = ne;
+                    q.push({nr, nc, nmask, ne});
+                }
+            }
+
+            moves++;
+        }
+
+        return -1;
+    }
+};
